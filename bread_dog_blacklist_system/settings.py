7f5ec09e51269b1d2401b596de2922362af46521
Qianyiovo <qianyiovo@outlook.com>
2022-11-16 11:01:48 +0800

    🔧 Update settings.py

--- bread_dog_blacklist_system/settings.py
+++ bread_dog_blacklist_system/settings.py
@@ -117,13 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
-else:
-    STATIC_ROOT = "/var/www/example.com/static/"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = "/var/www/example.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
