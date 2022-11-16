91e2d9e9a5cb6267594ebe515c22fce50e90c3f1
Qianyiovo <qianyiovo@outlook.com>
2022-11-16 11:09:12 +0800

    🔧 Update settings.py

--- bread_dog_blacklist_system/settings.py
+++ bread_dog_blacklist_system/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gt7@it6_e7g!4s3&f44+zyk*k80%o$q+q(!2431$n9zy^n^3ch'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
